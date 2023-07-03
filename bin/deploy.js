ef025ef7c727575b8d30721e0d27efcf913cf840
Peter Hua <peter.s.hua@gmail.com>
2023-07-03 13:37:21 -0700

    Add random secret generation
    Co-authored-by: Jared Naylor <94658559+jared-naylor@users.noreply.github.com>
    Co-authored-by: Sho Sugihara <66851584+W-Sho-Sugihara@users.noreply.github.com>

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,39 +1,130 @@
-const { CloudFormation } = require("@aws-sdk/client-cloudformation");
-const fs = require("fs");
-const readline = require('readline');
+#! /usr/bin/env node
+import yargs from "yargs";
+import chalk from "chalk";
 
+import {
+  CloudFormation,
+  RegistrationStatus,
+} from "@aws-sdk/client-cloudformation";
+import fs from "fs";
+import util from "util";
+import readline from "readline";
+import cryptoRandomString from "crypto-random-string";
+
+const tinkerPurple = chalk.rgb(99, 102, 241);
+
+const awsRegions = [
+  "us-east-1",
+  "us-east-2",
+  "us-west-1",
+  "us-west-2",
+  "af-south-1",
+  "ap-east-1",
+  "ap-south-1",
+  "ap-northeast-2",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ap-northeast-1",
+  "ca-central-1",
+  "eu-central-1",
+  "eu-west-1",
+  "eu-west-2",
+  "eu-west-3",
+  "eu-north-1",
+  "me-south-1",
+  "sa-east-1",
+];
+
+const stackName = "TinkerAdminStack";
+const minSecretLength = 35;
 const templatePath = "./tinker_admin_template.json";
+const encoding = "utf8";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// TODO: Change to promises instead of callbacks?
+const readFileAsync = util.promisify(fs.readFile);
 
-rl.question("Enter the region: ", (region) => {
-  const cloudformation = new CloudFormation({ region });
+const readTemplateFromFile = async (templatePath, encoding) => {
+  try {
+    let template = await readFileAsync(templatePath, encoding);
+    return template;
+  } catch (error) {
+    console.error(chalk.red("Error reading template file:"), error);
+    rl.close();
+    process.exit(1);
+  }
+};
 
-  fs.readFile(templatePath, "utf8", (err, template) => {
-    if (err) {
-      console.error("Error reading template file:", err);
-      rl.close();
-      process.exit(1);
-    }
+const promisifyRegionQuestion = () => {
+  return new Promise((resolve, reject) => {
+    rl.question(tinkerPurple("Enter the region: "), (answer) => {
+      if (!awsRegions.includes(answer)) {
+        reject(chalk.red("Invalid region"));
+      }
 
-    const stackParams = {
-      StackName: "TinkerAdminStack",
-      TemplateBody: template,
-    };
+      resolve(answer);
+    });
+  });
+};
 
-    cloudformation.createStack(stackParams, (err, data) => {
-      if (err) {
-        console.error("Error creating stack:", err);
-        process.exit(1);
+const askRegion = async () => {
+  try {
+    let region = await promisifyRegionQuestion();
+    return region;
+  } catch (error) {
+    console.error(error);
+    rl.close();
+    process.exit(1);
+  }
+};
+
+const promisifyCreateStack = async (cloudFormation, stackParams) => {
+  return new Promise((resolve, reject) => {
+    cloudFormation.createStack(stackParams, (error, data) => {
+      if (error) {
+        reject(error);
       } else {
-        console.log("Stack creation initiated:", data.StackId);
-        process.exit(0);
+        resolve(data);
       }
     });
   });
+};
+
+const createStack = async (cloudFormation, stackParams) => {
+  try {
+    const data = await promisifyCreateStack(cloudFormation, stackParams);
+    console.log(tinkerPurple("Stack creation initiated:"), data.StackId);
+    process.exit(0);
+  } catch (err) {
+    console.error(chalk.red("Error creating stack:"), err);
+    process.exit(1);
+  }
+};
+
+let template = await readTemplateFromFile(templatePath, encoding);
+let region = await askRegion();
+
+let secret = cryptoRandomString({
+  length: minSecretLength,
+  type: "alphanumeric",
 });
+
+console.log(tinkerPurple("Your secret is"), secret);
+
+const cloudFormation = new CloudFormation({ region });
+
+const stackParams = {
+  StackName: stackName,
+  TemplateBody: template,
+  Parameters: [
+    {
+      ParameterKey: "Secret",
+      ParameterValue: secret,
+    },
+  ],
+};
+
+await createStack(cloudFormation, stackParams);

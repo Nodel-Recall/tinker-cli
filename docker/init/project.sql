d8edba0bf3f53fbcc83ca827106b12fa8168f4fd
Peter Hua <37195345+PeterSHua@users.noreply.github.com>
2023-07-04 11:26:08 -0700

    Copy sql scripts from frontend repo
    Co-authored-by: Jared Naylor <94658559+jared-naylor@users.noreply.github.com>
    Co-authored-by: Sho Sugihara <66851584+W-Sho-Sugihara@users.noreply.github.com>

--- docker/init/project.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE project (
-    name             VARCHAR(40)  NOT NULL CONSTRAINT only_chars CHECK (name ~ '^[a-zA-Z]+$'),
-    ip               INET         NOT NULL,
-    secret           VARCHAR(40)  NOT NULL,
-    anon_jwt         VARCHAR(300) NOT NULL,
-    service_role_jwt VARCHAR(300) NOT NULL
-);

bb534c5ade3e72d3aa0cc943a5e01782bf44720a
Sho Sugihara <shosmail31@gmail.com>
2023-07-12 11:01:25 -0700

    added constraints function to projects_rpc file

--- docker/init/project_rpc.sql
+++ docker/init/project_rpc.sql
@@ -11,3 +11,29 @@ CREATE OR REPLACE FUNCTION get_columns_from_table(p_table_name TEXT)
   END;
   $$ LANGUAGE plpgsql;
 
+-- get the constraints of each column in a table
+CREATE OR REPLACE FUNCTION get_column_constraints(p_table_name TEXT)
+  RETURNS TABLE (column_name TEXT, constraint_name TEXT, nullable TEXT, constraint_type TEXT)
+  AS $$
+  BEGIN
+    RETURN QUERY
+    SELECT
+        col.column_name::TEXT,
+        cons.constraint_name::TEXT,
+        CASE
+            WHEN col.is_nullable = 'NO' THEN 'NOT NULL'
+            ELSE ''
+        END,
+        cons.constraint_type::TEXT
+    FROM
+        information_schema.columns AS col
+    LEFT JOIN
+        information_schema.constraint_column_usage AS ccu ON col.column_name = ccu.column_name AND col.table_name = ccu.table_name
+    LEFT JOIN
+        information_schema.table_constraints AS cons ON ccu.constraint_name = cons.constraint_name
+    WHERE
+        col.table_name = p_table_name;
+  END;
+  $$ LANGUAGE plpgsql;
+
+-- create new table
